cf77633ad5fe8a5cf36f55ec602bd16336693d96
EmilyVidaurri <emily.vidaurri12@gmail.com>
2020-01-22 23:18:29 +0000

    corrected my life???

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,38 @@
-<html>
-    <head>
-        <title>My Flask Site</title>
-    </head>
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/', methods=['GET'])
+def main():
+    hostname = request.host_url
+    message = 'BYEEEEE'
+    return render_template('main.html', host=hostname, msg=message)
+
+@app.route('/about', methods=['GET'])
+def mission():
+    return render_template('about.html')
+@app.route('/newroute', methods=['GET'])
+def newroute():
+    return "Joe Momma"
+
+
+@app.route('/calculator', methods=['GET', 'POST'])
+def calculator():
+    if request.method == 'POST':
+        fav_num = int(request.form['number'])
+        return f'The square of your favorite number is {fav_num ** 2}'
+
+    return """
+    <html>
     <body>
-        <h1>Welcome to this website!</h1>
-        <div><a href="{{ host }}about">About</a></div>
-        <p>{% if msg %} {{ msg }} {% endif %}</p>
+      <form method="post">
+        <label for="name">Name</label>
+        <input id="name" name="name" type="text">
+        <label for="number">Favorite number</label>
+        <input id="number" name="number" type="text">
+        <input type="submit" value="Submit">
+      </form>
     </body>
-</html>
+    </html>
+    """
