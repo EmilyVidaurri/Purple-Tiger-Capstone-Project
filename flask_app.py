2c33888e5662a02988e1306945e85077750055c5
EmilyVidaurri <emily.vidaurri12@gmail.com>
2020-02-05 22:41:20 +0000

    modified flask

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,7 @@ app.config['DEBUG'] = True
 def main():
     hostname = request.host_url
     message = 'BYEEEEE'
-    return render_template('main.html', host=hostname, msg=message)
+    return render_template('mai.html', host=hostname, msg=message)
 
 @app.route('/about', methods=['GET'])
 def mission():
@@ -17,22 +17,3 @@ def newroute():
     return "Joe Momma"
 
 
-@app.route('/calculator', methods=['GET', 'POST'])
-def calculator():
-    if request.method == 'POST':
-        fav_num = int(request.form['number'])
-        return f'The square of your favorite number is {fav_num ** 2}'
-
-    return """
-    <html>
-    <body>
-      <form method="post">
-        <label for="name">Name</label>
-        <input id="name" name="name" type="text">
-        <label for="number">Favorite number</label>
-        <input id="number" name="number" type="text">
-        <input type="submit" value="Submit">
-      </form>
-    </body>
-    </html>
-    """
